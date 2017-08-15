a30861fb574904b4f591b250acd9ba646d7a0b69
Ugotsta.com <ugotsta@gmail.com>
2017-08-15 16:00:28 +0000

    Inital changes.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 var TOC = [];
 var gist;
 var document_content;
+var current_slide = 0;
+var total_slides = 0;
 jQuery(document).ready(function() {
     
     // get url parameters
@@ -24,7 +26,7 @@ jQuery(document).ready(function() {
     
     var gist = getURLParameter('gist');
     var filename = getURLParameter('filename');
-    if (!gist) gist = 'f1ff10976bd1e43445b19af9fd5bd311';
+    if (!gist) gist = '4cef45f3d84ef7de915e526ef0e64cca';
     $.ajax({
         url: 'https://api.github.com/gists/' + gist,
         type: 'GET',
@@ -40,49 +42,47 @@ jQuery(document).ready(function() {
                     }
                 }
             }
-        }
-        else {
+        } else {
             objects.push(gistdata.data.files[filename].content);
         }
-        document_content = objects[0];
-        render(document_content);
+        render( objects[0] );
         render_sections();
-        position_sections();
-        render_connections();
+        check_css();
         render_info();
         register_keys();
-        if (gist === 'f1ff10976bd1e43445b19af9fd5bd311') $('#header h1').attr('id', 'cheats');
     }).error(function(e) {
         console.log('Error on ajax return.');
     });
     
-    // allow for custom CSS via Gist
-    var css = getURLParameter('css');
-    var cssfilename = getURLParameter('cssfilename');
-    if (css) {
-        $.ajax({
-            url: 'https://api.github.com/gists/' + css,
-            type: 'GET',
-            dataType: 'jsonp'
-        }).success(function(gistdata) {
-            var objects = [];
-            if (!cssfilename) {
-                for (var file in gistdata.data.files) {
-                    if (gistdata.data.files.hasOwnProperty(file)) {
-                        var o = gistdata.data.files[file].content;
-                        if (o) {
-                            objects.push(o);
+    function check_css() {
+        // allow for custom CSS via Gist
+        var css = getURLParameter('css');
+        var cssfilename = getURLParameter('cssfilename');
+        if (css) {
+            $.ajax({
+                url: 'https://api.github.com/gists/' + css,
+                type: 'GET',
+                dataType: 'jsonp'
+            }).success(function(gistdata) {
+                var objects = [];
+                if (!cssfilename) {
+                    for (var file in gistdata.data.files) {
+                        if (gistdata.data.files.hasOwnProperty(file)) {
+                            var o = gistdata.data.files[file].content;
+                            if (o) {
+                                objects.push(o);
+                            }
                         }
                     }
                 }
-            }
-            else {
-                objects.push(gistdata.data.files[filename].content);
-            }
-            render_css(objects[0]);
-        }).error(function(e) {
-            console.log('Error on ajax return.');
-        });
+                else {
+                    objects.push(gistdata.data.files[filename].content);
+                }
+                render_css(objects[0]);
+            }).error(function(e) {
+                console.log('Error on ajax return.');
+            });
+        }
     }
     
     function render_css(css) {
@@ -93,19 +93,17 @@ jQuery(document).ready(function() {
     }
 
     function render(content) {
-        var md = window.markdownit();
-        $('#wrapper').html( md.render(content) );
-    }
-    
-    function register_keys() {
-        // Add listeners for keypress commands
-        $(document).keypress(function(e) {
-            if( e.which == 104 || e.which == 63 || e.which == 72 || e.which == 47 ) {
-                $('#info').toggle();
-            } else if( e.which == 88 || e.which == 120) {
-                open_export();
-            }
+        var md = window.markdownit({
+            html: true, // Enable HTML tags in source
+            xhtmlOut: true, // Use '/' to close single tags (<br />).
+            breaks: false, // Convert '\n' in paragraphs into <br>
+            langPrefix: 'language-', // CSS language prefix for fenced blocks.
+            linkify: true,
+            typographer: true,
+            quotes: '“”‘’'
         });
+        content = content.replace(/[:;]/g, ':<br />');
+        $('#wrapper').html( md.render( content ) );
     }
     
     function render_sections() {
@@ -113,129 +111,47 @@ jQuery(document).ready(function() {
         // remove any empty p elements
         $( 'p:empty' ).remove();
         
-        // header section
-        if ( $('#wrapper ' + header).length ) {
-            $('#wrapper ' + header).each(function() {
-                $(this).nextUntil(heading).andSelf().wrapAll('<section id="header"/>');
-                $(this).wrapInner('<a name="header"/>');
-            });
-        } else {
-            //no header, so we'll add an empty one
-            $('#wrapper').prepend('<section id="header"></section>');
-        }
+        // remove br tags
+        //$('br').remove();
         
-        // command sections
-        $('#wrapper ' + heading).each(function() {
-            var name = $(this).text().toLowerCase().replace(/\s/g, "-");
-            // remove any existing commas
-            name = name.replace(',', '');
-            // remove footnote reference from name
-            var splice = name.split('[');
-            name = name.split('[')[0];
-            
-            // add name as id for respective section
-            $(this).nextUntil(heading).andSelf().wrapAll( '<div class="section" id="' + name + '"/>' );
-            
-            // handler for footnote sections
-            if ( splice.length > 1 ) {
-                var num = splice[1].split(']')[0];
-                var text = $(this).text();
-                $(this).text( text.split('[')[0] );
-                $(this).parent().addClass( 'note note-' + Number( num ) );
-                $(this).wrapInner( '<a class="handle" name="' + name + '"/>' );
-                $(this).nextUntil(heading).wrapAll('<div class="content"/>');
-            } else {
-                // handler for other sections
-                var handle = '<a class="handle" name="' + name + '">' + $(this).text() + '</a>';
-                var $p = $(this).nextUntil(heading).wrapAll('<div class="content"/>');
-                $p.first('p').prepend(handle);
+        var counter = 0;
+        $('#wrapper').children().each(function() {
+            if ( $(this).is('p') ) {
+                $(this).html(function(i, html){
+                    // split at br tags and wrap content with slide divs
+                     var txt = html.split('<br>');
+                     for( var x = 0; x < txt.length; x++ ) {
+                         $('#wrapper').append( '<div id="slide-' + counter + '" class="slide"><p>' + txt[x] + "</p></div>" );
+                         counter++;
+                     }
+                });
+                // p tag contents moved to wrapper so remove original now
                 $(this).remove();
+            } else {
+                $(this).wrap( '<div id="slide-' + counter + '" class="slide" />' );
+                counter++;
             }
         });
         
-        // wrap all command sections in new section
-        $('#header').siblings().wrapAll('<section id="commands"/>');
+        total_slides = counter - 1;
+        $('#slide-' + current_slide).addClass('current');
         
-        // update [1] footnote links
-        $('.section em').each(function() {
-            var text = $(this).text();
-            var splice = text.split('[');
-            var num = splice[1].split(']')[0];
-            $(this).text( splice[0] );
-            $(this).addClass( 'n-' + Number( num ) );
-        });
-        
-        // hide all other sections if showonly has been specified
-        if(showonly != '') {
-            $('#' + showonly).siblings().hide();
-        }
-        
-        $( '.section' ).draggable({
-            drag: function() { render_connections(); },
-            stop: function() { render_connections(); }
-        });
-
-        $( '.section' ).resizable({
-            resize: function() { render_connections(); },
-            stop: function() { render_connections(); }
-        });
     }
     
-    function position_sections() {
-        var docwidth = $(document).width();
-        var $sections = $('.section *:contains("<!--")');
-        if ( $sections.length > 0 ) {
-            $sections.each(function() {
-                // extract attributes
-                var text = $sections.text();
-                var s = text.substr(text.indexOf("<!-- {") + 6).split('}')[0];
-                var pairs = s.split(',');
-                for ( var i = 0; i < pairs.length; i++ ) {
-                    var key = pairs[i].split(':')[0];
-                    var value = pairs[i].split(':')[1];
-                    $(this).parent().css( key, value );
-                }
-                var html = $(this).html();
-                $(this).html( html.replace( /&lt;!--(.*?)--&gt;/, '') );
-            });
-        } else {
-            var counter = 0;
-            var left = 0;
-            var top = 0;
-            $sections = $('.section');
-            $sections.each(function() {
-                // set default values for section positions
-                if ( counter > 0 ) {
-                    var prev_width = $(this).prev('.section').width();
-                    // increment height if width of document is surpassed
-                    if ( left > docwidth - prev_width * 2 ) {
-                        left = 0;
-                        top += $(this).prev('.section').height();
-                    } else {
-                        left += prev_width;
-                    }
-                    $(this).css( {top: top, left: left} );
-                }
-                counter += 1;
-            });
-        }
-    }
-    
-    function render_connections() {
-        if ( $('connection').length === 0 ) {
-            $( '.section .content [class^="n"]' ).each(function() {
-                var classes = $(this).attr('class');
-                var to = classes.substr(classes.indexOf("n-") + 2).split(' ')[0];
-                $(this).connections({ to: '.note-' + to , 'class': 'c-' + to});
-            });
-        } else {
-            // update connections
-            $('.section .content [class^="n"]').connections('update');
-        }
+    function toc_html() {
+        var html = '';
+        // iterate section classes and get id name to compose TOC
+        $( '.slide' ).each(function() {
+            var name = $( this ).attr( 'id' );
+            name = name.split('[')[0];
+            html += '<a href="#' + name + '">';
+            html += name;
+            html += '</a>';
+        });
+        return html;
     }
     
     function render_info() {
-        
         // render TOC
         $('#toc').html( toc_html() );
         
@@ -252,74 +168,46 @@ jQuery(document).ready(function() {
         $('#gist-url').html('<a href="' + url + '">' + gist + '</a>');
     }
     
-    function toc_html() {
-        var html = '';
-        // iterate section classes and get id name to compose TOC
-        $( '#commands .section' ).each(function() {
-            var name = $( this ).attr( 'id' );
-            name = name.split('[')[0];
-            html += '<a href="#' + name + '">';
-            html += name;
-            html += '</a>';
-        });
-        return html;
+    function change_slide() {
+        $('#slide-' + current_slide).addClass('current fade');
     }
     
-    function open_export() {
-        // open new window
-        var xWindow = window.open(gist);
-        var content = '';
-        var newline = '\n\n'//'<br/>';
-        
-        // replace em content, we'll revert it back after
-        $('.section .content em').each(function() {
-            var id = $(this).attr('class');
-            id = id.split('-')[1];
-            var text = $(this).text();
-            $(this).replaceWith( '_' + text + '[' + id + ']_' );
-        });
-        
-        // iterate over all sections to get content
-        $('.section').each(function() {
-            // get section attributes
-
-            if ( $(this).hasClass('note') ) {
-                var classes = $(this).attr('class');
-                var id = classes.substr( classes.indexOf("note-") + 5 ).split(' ')[0];
-                content += '## ' + $(this).find('a.handle').text() + '[' + id + ']';
-                content += newline;
-                var li = $(this).find('ul li').text();
-                content += '- ' + li + newline;
-                content += $(this).find('p').text();
-            } else {
-                var h = $(this).find('a.handle').text();
-                content += '## ' + h;
-                content += newline;
-                var $p = $(this).find('p');
-                var replace = $p.text().replace( h, '');
-                content += replace;
+    function register_keys() {
+        // Add listeners for keypress commands
+        $(document).keydown(function(e) {
+            
+            switch ( e.which ) {
+                case 104:
+                case 63:
+                case 72:
+                case 47:
+                    $('#info').toggle();
+                    break;
+                case 109:
+                case 189:
+                case 100:
+                case 37:
+                    // LEFT
+                    $('#slide-' + current_slide).removeClass('current fade');
+                    current_slide--;
+                    if ( current_slide < 0 ) current_slide = total_slides;
+                    change_slide();
+                    break;
+                case 107:
+                case 187:
+                case 102:
+                case 39:
+                    // RIGHT
+                    $('#slide-' + current_slide).removeClass('current fade');
+                    current_slide++;
+                    if ( current_slide > total_slides ) current_slide = 0;
+                    change_slide();
+                    break;
+                default: 
+                    console.log("Default response for switch statement.");
             }
             
-            var attr = '';
-            var px = 'px';
-            attr += 'left:' + $(this).position().left + px;
-            attr += ',top:' + $(this).position().top + px;
-            attr += ',width:' + $(this).width() + px;
-            attr += ',height:' + $(this).height() + px;
-            
-            content += newline;
-            content += '&lt;!-- {' + attr + '} -->';
-            content += newline + newline;
         });
-        xWindow.document.write( content.replace(/\n\n/g, '<br/>') );
-        
-        // revert document
-        $('#wrapper').html('');
-        render(content);
-        render_sections();
-        position_sections();
-        render_connections();
-        render_info();
     }
-
+    
 });
